5b92201e971faaf93d5c0635ea848d6a8c379841
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-15 13:06:11 +0530

    Add Book class

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,16 @@
 public class Book{
-    public int Book_Id;
-    public String Title;
-    public String Author;
-    public int Total_Copies;
-    public int Available_Copies;
+    public int bookId;
+    public String title;
+    public String author;
+    public int totalCopies;
+    public int availableCopies;
+
+    public Book(int bookId, String title, String author, int totalCopies){
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.totalCopies = totalCopies;
+        availableCopies = totalCopies;
+    }
+
 }
