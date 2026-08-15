29006b56fa258ff67033f190b9e1cffdac6ca6cc
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-15 15:21:32 +0530

    Implement book creation and duplicate validation

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    public static void main(String[] args){
+
+    Library objLibrary = new Library();
+    Book book1 = new Book(101, "Rich dad poor dad", "Avadhut", 10);
+    Book book2 = new Book(102, "love ", "Avadhut Ingale", 5);
+    
+    objLibrary.addBook(book1);
+    objLibrary.addBook(book2);
+
+    Book book3 = new Book(101, "funny", "snehal", 1);
+    objLibrary.addBook(book3);
+    }
+
+}
