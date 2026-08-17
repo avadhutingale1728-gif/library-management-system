b324c53b03127e12ea99a5a170621186fa2c23a3
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-17 13:53:02 +0530

    Add proper comments in Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,16 @@
+/*
+  Represents a book available in the library.
+ 
+  Stores basic book information such as:
+  - Book ID
+  - Title
+  - Author
+  - Total number of copies
+  - Number of currently available copies
+ 
+  When a book is created, all copies are initially available.
+ */
+
 public class Book{
     public int bookId;
     public String title;
